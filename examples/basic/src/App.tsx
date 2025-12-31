ac51abae47742f490d80abb9100c1d390cc9ad57
Michael Hernando <mhernando334@gmail.com>
2025-12-31 17:28:16 -0500

    init

---
+++ examples/basic/src/App.tsx
@@ -0,0 +1,9 @@
+import { helloWorld } from '@inkblocks/core';
+
+function App() {
+	const message = helloWorld;
+
+	return <>{message}</>;
+}
+
+export default App;
---
+++ packages/core/vite.config.ts
@@ -0,0 +1,36 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import pkg from './package.json';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+	build: {
+		sourcemap: true,
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			name: 'inkblocks',
+			formats: ['es', 'cjs'],
+			fileName: (format) =>
+				format === 'es' ? 'inkblocks.js' : 'inkblocks.cjs',
+		},
+		rollupOptions: {
+			external: [
+				...Object.keys(pkg.dependencies ?? {}),
+				...Object.keys(
+					(
+						pkg as unknown as {
+							peerDependencies?: Record<string, string>;
+						}
+					).peerDependencies ?? {}
+				),
+			],
+			output: {
+				interop: 'compat',
+			},
+		},
+	},
+});
